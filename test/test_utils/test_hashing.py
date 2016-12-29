69e08025e9c782ce1533369669170f4fc6598092
Ben Granett <ben.granett@brera.inaf.it>
2016-12-29 21:40:30 +0100

    unit test for hdf5cat hashing

---
+++ test/test_utils/test_hashing.py
@@ -0,0 +1,35 @@
+import sys
+import numpy as np
+import os
+import pypelid.utils.hdf5tools as hdf5tools
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def check_hashing(algo='blake2', hash_length=32):
+    filename = os.tempnam()
+    storage = hdf5tools.HDF5Catalogue(filename, 'w', 
+                preallocate_file=False,
+                hash_algorithm=algo,
+                hash_length=hash_length)
+
+    storage.close()
+    logging.debug("wrote %s", filename)
+    hdf5tools.validate_hdf5_file(filename)
+    os.system('head %s'%filename)
+    os.unlink(filename)
+
+
+def test_hashing():
+    yield check_hashing, 'blake2', 2
+    yield check_hashing, 'blake2', 32
+    yield check_hashing, 'blake2', 64
+    yield check_hashing, 'blake2', 128
+
+    yield check_hashing, 'md5', 2
+    yield check_hashing, 'md5', 32
+    yield check_hashing, 'md5', 64
+    yield check_hashing, 'md5', 128
+    yield check_hashing, 'md5', 255
+
