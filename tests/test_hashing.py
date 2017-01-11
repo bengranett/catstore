0fce5c5dcf9dcd79f29705d9c66aafd588cf6a5e
Ben Granett <ben.granett@brera.inaf.it>
2017-01-11 16:18:01 +0100

    tests hdf5 hashing and ensures header is properly read and written.

--- tests/test_hashing.py
+++ tests/test_hashing.py
@@ -7,17 +7,17 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 
-def check_hashing(algo='blake2', hash_length=32):
+def check_hashing(algo='blake2', hash_length=32, hash_info_len=2):
     filename = os.tempnam()
-    storage = hdf5tools.HDF5Catalogue(filename, 'w', 
+    storage = hdf5tools.HDF5Catalogue(filename, 'w',
                 preallocate_file=False,
                 hash_algorithm=algo,
-                hash_length=hash_length)
+                hash_length=hash_length,
+                hash_info_len=hash_info_len)
 
     storage.close()
     logging.debug("wrote %s", filename)
-    hdf5tools.validate_hdf5_file(filename)
-    os.system('head %s'%filename)
+    hdf5tools.validate_hdf5_file(filename, hash_info_len=hash_info_len, hash_algo_len=1)
     os.unlink(filename)
 
 
@@ -32,4 +32,3 @@ def test_hashing():
     yield check_hashing, 'md5', 64
     yield check_hashing, 'md5', 128
     yield check_hashing, 'md5', 255
-
