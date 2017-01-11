b6b725477a18fa76a722d458d900384ba6b1acaa
Ben Granett <ben.granett@brera.inaf.it>
2017-01-11 17:07:05 +0100

    Updates catalouge store tests with preloaded and not preloaded data.

--- tests/test_catalogue_store.py
+++ tests/test_catalogue_store.py
@@ -37,8 +37,71 @@ def check_catalogue_store(n=100, zone_resolution=0):
 
 	with catalogue_store.CatalogueStore(filename, 'w', name='test',
 		zone_resolution=zone_resolution, preallocate_file=False) as cat:
-		# cat.preload(ra, dec)
-		# cat.allocate(dtypes)
+		cat.update(data)
+		cat.update_attributes(meta)
+		cat.update_units(units)
+		cat.update_description(description)
+
+	# Compute check sums
+	count = 0
+	check_lon = 0
+	check_lat = 0
+	with catalogue_store.CatalogueStore(filename) as cat:
+		print "zones:", len(cat.get_zones())
+		for group in cat.get_data():
+			lon, lat = np.transpose(group['skycoord'])
+			count += len(lon)
+			check_lon += np.sum(lon)
+			check_lat += np.sum(lat)
+	assert(count == n)
+	assert(np.allclose(check_lon, np.sum(skycoord[:, 0])))
+	assert(np.allclose(check_lat, np.sum(skycoord[:, 1])))
+
+	# delete the test file
+	os.unlink(filename)
+
+
+def check_catalogue_store_batches(n=100, zone_resolution=0):
+	""" Quick demo of catalogue store.
+
+	This routine loads the data in batches.
+
+	Generate mock data and write a catalogue store file.  Open the file and check
+	that contents are correct.
+	"""
+	# temporary file to use for test
+	filename = os.tempnam() + ".pypelid"
+
+	# generate distribution of points on the sky
+	ra, dec = sphere.sample_sphere(n)
+	skycoord = np.transpose([ra, dec])
+	redshift = np.random.uniform(1., 2., n)
+
+	# define metadata
+	data = {
+			'skycoord': skycoord,
+			'redshift': redshift,
+			}
+	dtypes = [
+			np.dtype([('skycoord', float, 2)]),
+			np.dtype([('redshift', float, 1)])
+			]
+	units = {'skycoord': 'degree', 'redshift': 'redshift'}
+	meta = {'coordsys': 'equatorial J2000'}
+	description = {'skycoord': 'RA and Dec coordinates', 'redshift': 'Cosmological redshift'}
+
+	batch = max(1, len(ra)//10)
+
+	with catalogue_store.CatalogueStore(filename, 'w', name='test',
+		zone_resolution=zone_resolution, preallocate_file=False) as cat:
+
+		# load in the catalogue in batches
+		i = 0
+		while i < len(ra):
+			cat.preprocess(ra[i:i+batch], dec[i:i+batch])
+			i += batch
+
+		cat.allocate(dtypes)
 		cat.update(data)
 		cat.update_attributes(meta)
 		cat.update_units(units)
@@ -77,6 +140,7 @@ def test_catalogue_store_2():
 	cases = [(10000, 0), (1000, 0), (1000, 2), (1, 0)]
 	for n, r in cases:
 		yield check_catalogue_store, n, r
+		yield check_catalogue_store_batches, n, r
 
 
 def test_catalogue_store_3():
@@ -86,4 +150,7 @@ def test_catalogue_store_3():
 	except ValueError:
 		pass
 
-check_catalogue_store(100,0)
+	try:
+		check_catalogue_store_batches(1, -1)
+	except ValueError:
+		pass
