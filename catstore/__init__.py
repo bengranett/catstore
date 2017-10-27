dbd0f7dfe9a81e2f710f4ab86f340f676beae83a
Ben Granett <ben.granett@brera.inaf.it>
2017-10-27 15:15:51 +0200

    update versioning

--- catstore/__init__.py
+++ catstore/__init__.py
@@ -1,13 +1,17 @@
-from metafil import GitEnv
+import subprocess
 
-__version__ = '?'
+__version__ = ''
+
 
 try:
     # if we are running in a git repo, look up the hash
-    __version__ = GitEnv(__file__).describe()
+    __version__ = subprocess.Popen(
+        ('git','--git-dir',os.path.dirname(__file__),'describe','--always'),
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
+    assert __version__
 except:
     # otherwise check for a version file
     try:
-        from . version import __version__
+        from . version import version as __version__
     except:
         pass
