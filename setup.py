08f825a8980b404952e61983749a6feb9761bb14
Ben Granett <benjamin.granett@unimi.it>
2019-08-02 13:17:06 +0200

    Removes minimask requirement from setup.py

--- setup.py
+++ setup.py
@@ -118,7 +118,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['h5py', 'h5py_cache', 'numpy', 'minimask'],
+    install_requires=['h5py', 'h5py_cache', 'numpy'],
 
     # # List additional groups of dependencies here (e.g. development
     # # dependencies). You can install these using the following syntax,
